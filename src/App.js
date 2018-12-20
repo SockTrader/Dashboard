278f0d2ec2c4881267c19d1988765c5782edde97
Wouter Cleymans <woutercleymans@gmail.com>
2018-12-20 07:52:26 +0100

    Revert typescript.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import websocket from 'websocket/lib/WebSocketClient';
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.module.scss';
 
 // debugger;
 console.log(websocket);
